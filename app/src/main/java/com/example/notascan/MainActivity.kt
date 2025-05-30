0c7a256fe18dce8f0a5d172e2d0c5252055045e8
Ana <anaclarasantos.amorin@gmail.com>
2025-05-30 13:21:34 -0300

    iniciando a HomeScreen

--- app/src/main/java/com/example/notascan/MainActivity.kt
+++ app/src/main/java/com/example/notascan/MainActivity.kt
@@ -3,45 +3,80 @@ package com.example.notascan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.notascan.ui.theme.NotaScanTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            NotaScanTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HomeScreen()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun HomeScreen() {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text("Notas") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF7BC9BF),
+                    titleContentColor = Color.White
+                ),
+                actions = {
+                    IconButton(onClick = { /* ação de busca */ }) {
+                        Icon(Icons.Default.Search, contentDescription = "Buscar", tint = Color.White)
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* ação da câmera */ },
+                containerColor = Color(0xFF7BBAC9)
+            ) {
+                Icon(Icons.Default.CameraAlt, contentDescription = "Adicionar com Câmera", tint = Color.White)
+            }
+        },
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Notas vazias",
+                        color = Color(0xFF7BC98A),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "Adione a sua primeira nota",
+                        color = Color(0xFF7BC9A4),
+                        fontSize = 16.sp
+                    )
+                }
+            }
+        }
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotaScanTheme {
-        Greeting("Android")
-    }
 }
